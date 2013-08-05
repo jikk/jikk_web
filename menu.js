80d91b17db2b39b41827c3aa275e8eee518d4f46
Kangkook Jee <aixer77@gmail.com>
2013-08-05 17:04:41 -0400

    update

---
+++ menu.js
@@ -0,0 +1,5 @@
+document.write('\
+<li><a href="index.html" accesskey="1" title="">Homepage</a></li>\
+<li><a href="pub.html" accesskey="2" title="">Publications</a></li>\
+<li><a href="teaching.html" accesskey="3" title="">Teaching</a></li>\
+');
