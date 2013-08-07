328d8e616671461787986031d8cfb8246b7823c6
Kangkook Jee <aixer77@gmail.com>
2013-08-07 17:40:52 -0400

    added sidebar.js

---
+++ sidebar.js
@@ -0,0 +1,27 @@
+document.write('\
+          <h2>Recent news</h2>\
+          <ul class="style2">\
+            <li class="first">\
+            <h3><a href="#">New paper</a></h3>\
+            Our new paper titled\
+            <p><a href="#"">ShadowReplica: Efficient Parallelization of Dynamic\
+        Data Flow Tracking"</a> is accepted to CCS 2013.\
+            </p>\
+            </li>\
+            <li>\
+            <h3><a href="#">One create python course site open</a></h3>\
+            <p>A course website for <a href="#">3101 programming lanaguage(Fall 2013)</a> is now ready.</p>\
+            </li>\
+            <li>\
+            <h3><a href="#">Fusce ultrices fringilla</a></h3>\
+            <p><a href="#">Quisque dictum integer nisl risus, sagittis\
+            convallis, rutrum id, congue, and nibh.</a></p>\
+            </li>\
+            <li>\
+            <h3><a href="#">Nulla luctus eleifend</a></h3>\
+            <p><a href="#">Quisque dictum integer nisl risus, sagittis\
+            convallis, rutrum id, congue, and nibh.</a></p>\
+            </li>\
+				</ul>\
+                <p><a href="#" class="button-style">Read More</a></p>\
+');
