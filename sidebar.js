1bf904cdef2533a425ed7977c06f96c7277a637f
Kangkook Jee <aixer77@gmail.com>
2013-08-08 12:12:21 -0400

    now files

--- sidebar.js
+++ sidebar.js
@@ -9,19 +9,13 @@ document.write('\
             </p>\
             </li>\
             <li>\
-            <h3><a href="#">One create python course site open</a></h3>\
-            <p>A course website for <a href="#">3101 programming lanaguage(Fall 2013)</a> is now ready.</p>\
+            <h3><a href="#">Course website is now open</a></h3>\
+            <p>A course website for <a href="#">3101 programming lanaguage -- python (Fall 2013)</a> is now ready.</p>\
             </li>\
             <li>\
-            <h3><a href="#">Fusce ultrices fringilla</a></h3>\
-            <p><a href="#">Quisque dictum integer nisl risus, sagittis\
-            convallis, rutrum id, congue, and nibh.</a></p>\
+            <h3><a href="#">New research project</a></h3>\
+            <p><a href="#">A comparison study of DBI frameworks</a></p>\
             </li>\
-            <li>\
-            <h3><a href="#">Nulla luctus eleifend</a></h3>\
-            <p><a href="#">Quisque dictum integer nisl risus, sagittis\
-            convallis, rutrum id, congue, and nibh.</a></p>\
-            </li>\
-				</ul>\
-                <p><a href="#" class="button-style">Read More</a></p>\
+			</ul>\
+            <p><a href="#" class="button-style">Read More Newes</a></p>\
 ');
