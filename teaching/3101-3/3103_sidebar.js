167a456f8a5dcc4292851d02653af73db08bfead
Kangkook Jee <aixer77@gmail.com>
2013-08-09 22:54:45 -0400

    javascripts

---
+++ teaching/3101-3/3103_sidebar.js
@@ -0,0 +1,20 @@
+document.write('\
+          <h2>Course news</h2>\
+          <ul class="style2">\
+            <li class="first">\
+            <h3><a href="#">New paper</a></h3>\
+            Our new paper titled\
+            <p><a href="#"">ShadowReplica: Efficient Parallelization of Dynamic\
+        Data Flow Tracking"</a> is accepted to CCS 2013.\
+            </p>\
+            </li>\
+            <li>\
+            <h3><a href="#">Course website is now open</a></h3>\
+            <p>A course website for <a href="#">3101 programming lanaguage -- python (Fall 2013)</a> is now ready.</p>\
+            </li>\
+            <li>\
+            <h3><a href="#">New research project</a></h3>\
+            <p><a href="#">A comparison study of DBI frameworks</a></p>\
+            </li>\
+			</ul>\
+');
